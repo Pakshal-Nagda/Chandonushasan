f62038ce38beefa7b7115be546a81f41c213b444
Pakshal-Nagda <pakshalnnagda@gmail.com>
2025-10-18 15:47:45 +0530

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -14,7 +14,7 @@ def raw2cleaned(filelist):
         cleaned = re.sub(r'।\s*।', '॥', cleaned)
         cleaned = re.sub(r'(([॥।])([०१२३४५६७८९\s.]*)\2)', cache_num, cleaned)
         cleaned = re.sub(r'यथा\s*', 'यथा\n', cleaned, flags=re.DOTALL)
-        cleaned = re.sub(r'(छंदोनुशासन|छन्दोऽनुशासनम्)[।\s]*', '', cleaned)
+        cleaned = re.sub(r'(छंदोनुशासन|छन्दोऽनुशासनम्)[।\s\n]+', '', cleaned, flags=re.DOTALL)
         cleaned = cleaned\
             .replace('॥', '॥\n')\
             .replace('।', '।\n')\
