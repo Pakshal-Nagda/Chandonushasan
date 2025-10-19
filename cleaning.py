632fe28a06b18cb5244e4a721a38f77f60b65d77
Pakshal-Nagda <pakshalnnagda@gmail.com>
2025-10-19 17:03:14 +0530

    Added renumbering

--- cleaning.py
+++ cleaning.py
@@ -23,7 +23,7 @@ def raw2cleaned(filelist):
             text = f.read()
         cleaned = re.sub(r'[^\u0900-\u0970\s.]', '', text)
         cleaned = re.sub(r'।\s*।', '॥', cleaned)
-        cleaned = re.sub(r'($|\s+)यथा\s*', ' यथा\n', cleaned, flags=re.DOTALL)
+        #cleaned = re.sub(r'(\s+)यथा\s*', ' यथा\n', cleaned, flags=re.DOTALL)
         cleaned = re.sub(r'(\s*([॥।])([०१२३४५६७८९\s.]*)\2)', cache_num, cleaned)
         cleaned = re.sub(r'(छंदोनुशासन|छन्दोऽनुशासनम्|द्वितीयोऽध्यायः)[।\s]+', '', cleaned, flags=re.DOTALL)
         cleaned = re.sub(r'[.०१२३४५६७८९]', '', cleaned)
@@ -32,7 +32,7 @@ def raw2cleaned(filelist):
             .replace('।', '।\n')\
             .replace('|', '॥')\
             .replace('_', '.')
-        cleaned = re.sub(r'(\n\s*)+', '\n', cleaned)
+        cleaned = re.sub(r'(\n[\d॥।\s.]*)+', '\n', cleaned)
         with open('cleaned/' + file, 'w') as f:
             f.write(cleaned)
 
@@ -43,10 +43,55 @@ def extract_ch2(filelist):
         start = 'उक्तायां गः श्रीः'
         end = 'दण्डकप्रकरणम्'
         ch2 = text[text.find(start): text.find(end) + 16]
+        ch2 = re.sub(r'\n', ' ', ch2, flags=re.DOTALL)
+        ch2 = re.sub(r'(॥[\d.]+॥|[॥।]) ', lambda x: x.group(1) + '\n', ch2)
+        ch2 = re.sub(r'(?<=[॥।]\n)\s*यथा\s*', 'यथा\n', ch2, flags=re.DOTALL)
         with open('ch2/' + file, 'w') as f:
             f.write(ch2)
+
+def renumber(filelist):
+    for file in filelist:
+        with open('ch2/' + file, 'r') as f:
+            text = f.read()
+
+        max_num = 6
+        buffer = None
+        corrections = []
+        for match in re.finditer(r'[\d.]+', text):
+            try:
+                number = float(match.group())
+            except:
+                number = -1
+            if max_num <= number < max_num + 3:
+                if buffer:
+                    prev, next = max_num, number
+                    if prev % 1 == 0 and next == prev + 1:
+                        correction = f'{int(prev)}.1'
+                    elif int(next) == int(prev) + 1:
+                        correction = f'{int(next)}'
+                    elif int(next) == int(prev) + 2:
+                        correction = f'{int(prev) + 1}'
+                    else:
+                        correction = '?'
+                    corrections.append((buffer, correction))
+                    buffer = None
+                max_num = number
+            else:
+                buffer = match
+
+        diff = 0
+        for match, correct in corrections:
+            start, end = match.span()
+            start += diff
+            end += diff
+            diff += len(correct) - (end - start)
+            text = text[:start] + correct + text[end:]
+
+        with open('ch2/' + file, 'w') as f:
+            f.write(text)
 
 if __name__ == '__main__':
     filelist = ['sq.txt', 'si.txt', 'ch.txt']
     raw2cleaned(filelist)
     extract_ch2(filelist)
+    renumber(filelist[:-1])
